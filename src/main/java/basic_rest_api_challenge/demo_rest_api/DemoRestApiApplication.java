9b67573191728452dd9bc33c3dc2efd0b1a265ef
Austin <austinvarnon@gmail.com>
2021-01-22 11:57:39 -0600

    first commit

---
+++ src/main/java/basic_rest_api_challenge/demo_rest_api/DemoRestApiApplication.java
@@ -0,0 +1,13 @@
+package basic_rest_api_challenge.demo_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/basic_rest_api_challenge/demo_rest_api/DemoRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package basic_rest_api_challenge.demo_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
