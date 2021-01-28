a84ff1ffd801edae67e7f78ab5bce2cae4685f91
Austin <austinvarnon@gmail.com>
2021-01-28 11:06:32 -0600

    Job controller done, testing next

--- src/main/java/basic_rest_api_challenge/demo_rest_api/Controllers/JobController.java
+++ src/main/java/basic_rest_api_challenge/demo_rest_api/Controllers/JobController.java
@@ -1,7 +1,81 @@
 package basic_rest_api_challenge.demo_rest_api.Controllers;
 
+import basic_rest_api_challenge.demo_rest_api.Exception.ResourceNotFoundException;
+import basic_rest_api_challenge.demo_rest_api.Models.Job;
+import basic_rest_api_challenge.demo_rest_api.Services.ExceptionService;
+import basic_rest_api_challenge.demo_rest_api.Services.JobService;
+import basic_rest_api_challenge.demo_rest_api.Services.SeedListService;
+import basic_rest_api_challenge.demo_rest_api.repos.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class JobController {
+
+    @Autowired
+    private SeedListService seedListService;
+
+    @Autowired
+    private ExceptionService exceptionService;
+
+    @Autowired
+    private JobService jobService;
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    //Start
+    @GetMapping("")
+    public void start(){
+        seedListService.jobSeedList();
+        seedListService.personSeedList();
+    }
+
+    //createJob
+    @PostMapping("/job")
+    public Job createJob(@RequestBody Job job){
+        return jobService.addJob(job);
+    }
+
+    //Show Job(s)
+    @GetMapping(value = "/jobs")
+    public List<Job> getAllJobs(){
+        return jobService.getJobs();
+    }
+
+    //get Job by Id
+    @GetMapping("/job/{id}")
+    public ResponseEntity<Job> getJobById(@PathVariable(value = "id") long id) throws ResourceNotFoundException{
+        Job job = jobService.getJob(id);
+        exceptionService.checkJobIsNull(job);
+        return ResponseEntity.ok().body(job);
+    }
+
+    //update Job
+    @PutMapping("/job/{id}")
+    public ResponseEntity<Job> updateJob(@PathVariable(value = "id") long id, @RequestBody Job jobDetails) throws ResourceNotFoundException{
+        Job job = jobService.getJob(id);
+        exceptionService.checkJobIsNull(job);
+
+        job.setJobTitle(jobDetails.getJobTitle());
+        job.setSalary(jobDetails.getSalary());
+        jobRepository.save(job);
+        return ResponseEntity.ok().body(job);
+    }
+
+    //deleteJob
+    @DeleteMapping("/job/{id}")
+    public ResponseEntity<Job> deleteJob(@PathVariable(value = "id") long id) throws ResourceNotFoundException{
+        Job job = jobService.getJob(id);
+        exceptionService.checkJobIsNull(job);
+        jobService.deleteJob(job);
+        return ResponseEntity.ok().build();
+    }
+
+
+
 }
--- src/main/java/basic_rest_api_challenge/demo_rest_api/Services/ExceptionService.java
+++ src/main/java/basic_rest_api_challenge/demo_rest_api/Services/ExceptionService.java
@@ -1,7 +1,9 @@
 package basic_rest_api_challenge.demo_rest_api.Services;
 
 import basic_rest_api_challenge.demo_rest_api.Exception.ResourceNotFoundException;
+import basic_rest_api_challenge.demo_rest_api.Models.Job;
 import basic_rest_api_challenge.demo_rest_api.Models.Person;
+import basic_rest_api_challenge.demo_rest_api.repos.JobRepository;
 import basic_rest_api_challenge.demo_rest_api.repos.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,9 @@ public class ExceptionService {
     @Autowired
     private PersonRepository personRepository;
 
+    @Autowired
+    private JobRepository jobRepository;
+
     public boolean checkPersonIsNull(Person person) throws ResourceNotFoundException{
         if(person == null){
             throw new ResourceNotFoundException("Person not found");
@@ -21,21 +26,19 @@ public class ExceptionService {
         }
     }
 
-    public boolean checkPersonIsNull(long personID) throws ResourceNotFoundException{
-        Person person = personRepository.findById(personID);
-        if(person == null){
-            throw new ResourceNotFoundException("Person not found");
-        } else {
-            return false;
-        }
-    }
-
-
     public ResponseEntity<Person> personNotFound(Person person) throws ResourceNotFoundException {
         if (person == null){
             throw new ResourceNotFoundException("Object not found");
         }
         return ResponseEntity.ok().body(person);
+    }
+
+    public boolean checkJobIsNull(Job job) throws ResourceNotFoundException{
+        if(job == null){
+            throw new ResourceNotFoundException("Job not found");
+        } else {
+            return false;
+        }
     }
 
 }
